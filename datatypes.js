3c6bce6d09f10c2e1191acf5b29c13d4ac8c6477
Ely Bourland <JEBourland@yahoo.com>
2021-01-01 10:07:59 -0800

    data types

---
+++ datatypes.js
@@ -0,0 +1,28 @@
+// PRIMITIVE TYPES
+
+// String
+const name = 'John Doe';
+// Number
+const age = 45;
+// Boolean
+const hasKids = true;
+// Null
+const car = null; // Will get an object in console.log - sort of a bug
+// Undefined
+let test;
+// Symbol
+const sym = Symbol();
+
+// REFERENCE TYPES - Objects
+
+// Array
+const hobbies = ['movies', 'music'];
+// Object literal
+const address = {
+    city: 'Boston',
+    state: 'MA'
+}
+const today = new Date();
+console.log(today);
+
+console.log(typeof today);
