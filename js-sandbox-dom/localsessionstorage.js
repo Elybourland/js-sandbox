6294a96f45828d020dfe1b06550a6b99e6ed06b9
Ely Bourland <JEBourland@yahoo.com>
2021-01-07 17:52:40 -0800

    local and session storage

---
+++ js-sandbox-dom/localsessionstorage.js
@@ -0,0 +1,43 @@
+// Set Local Storage Item
+// localStorage.setItem('name', 'John');
+// localStorage.setItem('age', '30');
+
+// Set Session Storage Item
+// sessionStorage.setItem('name', 'Beth');
+
+// Remove from storage
+// localStorage.removeItem('name');
+
+// Get from storage
+// const name = localStorage.getItem('name');
+// const age = localStorage.getItem('age');
+
+// Clear local storage
+// localStorage.clear();
+// console.log(name, age);
+
+document.querySelector('form').addEventListener('submit', function(e) {
+    const task = document.getElementById('task').value;
+
+    let tasks;
+
+    if(localStorage.getItem('tasks') === null) {
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+
+    tasks.push(task);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    alert('Task saved!');
+
+    e.preventDefault();
+});
+
+const tasks = JSON.parse(localStorage.getItem('tasks'));
+
+tasks.forEach(function(task) {
+    console.log(task);
+});
